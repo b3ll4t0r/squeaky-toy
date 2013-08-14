65b3962dd20f550e622223d000a0e90cd2065841
b3ll4t0r <billbraski1999@rocketmail.com>
2013-08-14 15:07:18 -0500

    Initial add.

--- README.md
+++ README.md
@@ -2,3 +2,5 @@ squeaky-toy
 ===========
 
 This is a Node.js module wrapper for Rexster and is currently geared toward Titan.
+
+For more details on Rexster please see: https://github.com/tinkerpop/rexster
---
+++ index.js
@@ -0,0 +1,14 @@
+/**
+ * This is the root for the Rexster wrapper.
+ *
+ * @author: <a href="mailto:billbraski1999@rocketmail.com">83||470r</a>
+ * Created on: Jupiter
+ */
+
+'use strict';
+
+module.exports = {
+    'edge': require('./edge'),
+    'vertex': require('./vertex')
+};
+//TODO: add functions to start Rexter and check if it's started
